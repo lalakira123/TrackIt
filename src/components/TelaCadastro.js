6f3244db48d680c04edd685eb7abd22dbb5e2a02
Laerte <laerteakira@gmail.com>
2022-03-31 08:19:23 -0300

    Post Tela Login + Cadastro

--- src/components/TelaCadastro.js
+++ src/components/TelaCadastro.js
@@ -1,18 +1,54 @@
-import { Link } from "react-router-dom";
+import { useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import axios from "axios";
 
 import Logo from "./../assets/img/Group 8.png";
 
 function TelaCadastro() {
+    const [cadastro, setCadastro] = useState({
+        email:"",
+        password:"",
+        name:"",
+        image:""
+    });
+
+    const navigate = useNavigate();
+
+    function cadastrar(e) {
+        e.preventDefault();
+        const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up",cadastro);
+        promise.then(() => {
+            navigate("/");
+        });
+        promise.catch(() => alert("Não foi possível cadastrar essa conta"));
+    }
+
     return (
         <Container>
            <img src={Logo} alt="Logo"/>
-           <Form>
-               <Input placeholder="email"/>
-               <Input placeholder="senha"/>
-               <Input placeholder="nome"/>
-               <Input placeholder="foto"/>
-               <Button>Cadastrar</Button>
+           <Form onSubmit={cadastrar}>
+               <Input 
+                placeholder="email" 
+                onChange={(e) => setCadastro({...cadastro, email: e.target.value})}
+                value={cadastro.email}
+                />
+               <Input 
+                placeholder="senha"
+                onChange={(e) => setCadastro({...cadastro, password: e.target.value})}
+                value={cadastro.password}
+                />
+               <Input 
+                placeholder="nome"
+                onChange={(e) => setCadastro({...cadastro, name: e.target.value})}
+                value={cadastro.name}
+                />
+               <Input 
+                placeholder="foto"
+                onChange={(e) => setCadastro({...cadastro, image: e.target.value})}
+                value={cadastro.image}
+                />
+               <Button type="submit">Cadastrar</Button>
            </Form>
            <Link to="/">
                 <Login>Já tem uma conta? Faça login!</Login>
--- src/components/TelaLogin.js
+++ src/components/TelaLogin.js
@@ -1,18 +1,44 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import axios from "axios";
 
 import Logo from "./../assets/img/Group 8.png";
 
 function TelaLogin() {
+    const [login, setLogin] = useState({
+        email:"",
+        password:""
+    });
+
+    const navigate = useNavigate();
+
+    function logar(e) {
+        e.preventDefault();
+        const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", login);
+        promise.then((resposta) => {
+            console.log(resposta.data);
+            navigate("/habitos");
+        });
+        promise.catch(() => alert("Não foi possível realizar o login"));
+    }
+
     return (
         <Container>
             <img src={Logo} alt="Logo"/>
-            <Form>
-                <Input placeholder="email"/>
-                <Input placeholder="senha"/>
-                <Link to="/habitos" >
-                    <Button>Entrar</Button>
-                </Link>
+            <Form onSubmit={logar}>
+                <Input 
+                    placeholder="email"
+                    onChange={(e) => setLogin({...login, email: e.target.value})}
+                    value={login.email}
+                />
+                <Input 
+                    placeholder="senha"
+                    onChange={(e) => setLogin({...login, password: e.target.value})}
+                    value={login.password}
+                />
+                <Button type="submit">Entrar</Button>
+
             </Form> 
             <Link to="/cadastro">
                 <Cadastro>Não tem uma conta? Cadastre-se!</Cadastro>
