46d905737bedabf37b79936b33be61418f4b99a5
Laerte <laerteakira@gmail.com>
2022-04-01 17:11:33 -0300

    Inclusão do Progress Bar no Footer

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -1,15 +1,33 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import 'react-circular-progressbar/dist/styles.css';
+
+import ConcluidoContext from "./../contexts/ConcluidoContext";
+
 function Rodape() {
+    const { concluido } = useContext(ConcluidoContext);
     return (
         <Footer>
             <Link to="/habitos">
                 <Habitos>Hábitos</Habitos>
             </Link>
-            <Link to="/hoje">
-                <p>Hoje</p>
-            </Link>
+            <Container>
+                <Link to="/hoje">
+                    <CircularProgressbar 
+                        value={concluido} 
+                        text={`Hoje`}
+                        styles={buildStyles({
+                            textColor: "#FFFFFF",
+                            textSize: "22px",
+                            trailColor: "none",
+                            pathColor: "#FFFFFF"
+                        })}
+                    />
+                </Link>
+            </Container>
             <Link to="/historico">
                 <Historico>Histórico</Historico>
             </Link>
@@ -32,10 +50,22 @@ const Footer = styled.div`
     color: #52B6FF;
 `
 
+const Container = styled.div`
+    margin-bottom: 50px;
+    width: 90px;
+    height: 90px;
+    background-color: #52B6FF;
+    border-radius: 100px;
+    border: 6px solid #52B6FF;
+    color: #FFFFFF;
+`
+
 const Habitos = styled.p`
     margin-left: 36px;
+    font-size: 18px;
 `
 
 const Historico = styled.p`
     margin-right: 26px;
+    font-size: 18px;
 `
